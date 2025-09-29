14ab2c9c585925a729c0c3bb6b9911ce094836e2
Yi Yang <sacriley@gmail.com>
2025-09-29 22:52:43 +0800

    delete resource

--- src/Core/FilamentToolsProvider.php
+++ src/Core/FilamentToolsProvider.php
@@ -15,29 +15,29 @@ class FilamentToolsProvider extends PackageServiceProvider
 
     public function packageBooted()
     {
-        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'ff-filament-tools');
+//        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'ff-filament-tools');
 
-        $this->publishes([
-            __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/ff-filament-tools')
-        ], 'views');
+//        $this->publishes([
+//            __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/ff-filament-tools')
+//        ], 'views');
 
-        Livewire::component('filament-tools::forms.components.file-upload/upload-button', \FF\FilamentTools\Forms\Components\FileUpload\UploadButton::class);
+//        Livewire::component('filament-tools::forms.components.file-upload/upload-button', \FF\FilamentTools\Forms\Components\FileUpload\UploadButton::class);
 
-        FilamentAsset::register([
-            AlpineComponent::make('file-upload', __DIR__ . '/../../resources/dist/forms/file-upload/file-upload.js'),
-            AlpineComponent::make('upload-button', __DIR__ . '/../../resources/dist/forms/file-upload/upload-button.js'),
-            AlpineComponent::make('upload-list', __DIR__ . '/../../resources/dist/forms/file-upload/upload-list.js'),
-        ], 'fanswoo/filament-tools/forms/file-upload');
+//        FilamentAsset::register([
+//            AlpineComponent::make('file-upload', __DIR__ . '/../../resources/dist/forms/file-upload/file-upload.js'),
+//            AlpineComponent::make('upload-button', __DIR__ . '/../../resources/dist/forms/file-upload/upload-button.js'),
+//            AlpineComponent::make('upload-list', __DIR__ . '/../../resources/dist/forms/file-upload/upload-list.js'),
+//        ], 'fanswoo/filament-tools/forms/file-upload');
 
         FilamentAsset::register([
             Css::make('core', __DIR__ . '/../../resources/dist/css/core.css'),
         ], 'fanswoo/filament-tools/css');
     }
 
-    public function configurePackage(Package $package): void
-    {
-        $package
-            ->name('filament-forms')
-            ->hasViews();
-    }
+//    public function configurePackage(Package $package): void
+//    {
+//        $package
+//            ->name('filament-forms')
+//            ->hasViews();
+//    }
 }
